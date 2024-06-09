396c3105d3ada4ac478ca72922928a966ae174b6
Fernandobrito95 <f.brito.castillo@gmail.com>
2024-06-09 19:30:12 -0400

    MantencionEntidadPersona

--- src/main/java/com/example/demo/model/PersonaDev.java
+++ src/main/java/com/example/demo/model/PersonaDev.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDev {
+public class PersonaDev {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
